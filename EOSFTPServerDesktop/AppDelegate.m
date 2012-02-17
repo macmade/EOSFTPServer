9bc3b2133bc4457f73183771b2538434a2c9b1db
Jean-David Gadina <macmade@eosgarden.com>
2012-02-17 01:30:11 +0100

    Added demo applications for Mac OS X and iOS.

---
+++ EOSFTPServerDesktop/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  EOSFTPServerDesktop
+//
+//  Created by Jean-David Gadina on 17.02.2012.
+//  Copyright (c) 2012 DigiDNA. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ EOSFTPServerMobile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EOSFTPServerMobile
+//
+//  Created by Jean-David Gadina on 17.02.2012.
+//  Copyright (c) 2012 DigiDNA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
